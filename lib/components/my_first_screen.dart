8bc2509167c6857e3143f2f85cc97c779bdf6f1e
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-11-19 14:35:23 +0900

    Image.asset, SingleChildScrollView

--- lib/components/my_first_screen.dart
+++ lib/components/my_first_screen.dart
@@ -12,31 +12,34 @@ class MyFirstScreen extends StatelessWidget {
         backgroundColor: Colors.lightBlue,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                print('클릭!!!');
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const MySecondScreen()),
-                );
-              },
-              child: const Text('두번째 화면으로'),
-            ),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.red,
-            ),
-            Image.network(
-              'https://img.khan.co.kr/news/2023/05/12/news-p.v1.20230512.e5fffd99806f4dcabd8426d52788f51a_P1.png',
-              width: 100,
-              height: 100,
-              fit: BoxFit.cover,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print('클릭!!!');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const MySecondScreen()),
+                  );
+                },
+                child: const Text('두번째 화면으로'),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Image.network(
+                'https://img.khan.co.kr/news/2023/05/12/news-p.v1.20230512.e5fffd99806f4dcabd8426d52788f51a_P1.png',
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+              Image.asset('assets/iu.jpg'),
+            ],
+          ),
         ),
       ),
     );
