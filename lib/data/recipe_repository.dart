f5441237c00410f80f55965ed4c12e7f62a6dfaa
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-11-20 13:24:59 +0900

    RecipeRepository 작성중

--- lib/data/recipe_repository.dart
+++ lib/data/recipe_repository.dart
@@ -1,3 +1,11 @@
+import 'model/recipe_models.dart';
+
+void main() {
+  final repository = RecipeRepository();
+  final recipes = repository.getRecipes();
+  print(recipes);
+}
+
 class RecipeRepository {
   final _recipeData = {
     "recipes": [
@@ -5,7 +13,8 @@ class RecipeRepository {
         "category": "Indian",
         "id": 1,
         "name": "Traditional spare ribs baked",
-        "image": "https://cdn.pixabay.com/photo/2017/11/10/15/04/steak-2936531_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2017/11/10/15/04/steak-2936531_1280.jpg",
         "chef": "Chef John",
         "time": "20 min",
         "rating": 4.0,
@@ -14,7 +23,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 3,
               "name": "Pork",
-              "image": "https://cdn.pixabay.com/photo/2019/12/20/14/44/meat-4708596_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2019/12/20/14/44/meat-4708596_1280.jpg"
             },
             "amount": 500
           },
@@ -22,7 +32,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 9,
               "name": "Onion",
-              "image": "https://cdn.pixabay.com/photo/2013/02/21/19/14/onion-bulbs-84722_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2013/02/21/19/14/onion-bulbs-84722_1280.jpg"
             },
             "amount": 50
           },
@@ -30,7 +41,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 8,
               "name": "Pepper",
-              "image": "https://cdn.pixabay.com/photo/2016/03/05/22/31/pepper-1239308_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2016/03/05/22/31/pepper-1239308_1280.jpg"
             },
             "amount": 10
           },
@@ -38,7 +50,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 1,
               "name": "Tomato",
-              "image": "https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg"
             },
             "amount": 100
           }
@@ -48,7 +61,8 @@ class RecipeRepository {
         "id": 2,
         "category": "Asian",
         "name": "Spice roasted chicken with flavored rice",
-        "image": "https://cdn.pixabay.com/photo/2018/12/04/16/49/tandoori-3856045_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2018/12/04/16/49/tandoori-3856045_1280.jpg",
         "chef": "Mark Kelvin",
         "time": "20 min",
         "rating": 4.0,
@@ -57,7 +71,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 6,
               "name": "Chicken",
-              "image": "https://cdn.pixabay.com/photo/2010/12/10/08/chicken-1140_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2010/12/10/08/chicken-1140_1280.jpg"
             },
             "amount": 300
           },
@@ -65,7 +80,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 4,
               "name": "Rice",
-              "image": "https://cdn.pixabay.com/photo/2016/02/29/05/46/brown-rice-1228099_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2016/02/29/05/46/brown-rice-1228099_1280.jpg"
             },
             "amount": 200
           },
@@ -73,7 +89,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 8,
               "name": "Pepper",
-              "image": "https://cdn.pixabay.com/photo/2016/03/05/22/31/pepper-1239308_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2016/03/05/22/31/pepper-1239308_1280.jpg"
             },
             "amount": 5
           },
@@ -81,7 +98,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 3,
               "name": "Pork",
-              "image": "https://cdn.pixabay.com/photo/2019/12/20/14/44/meat-4708596_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2019/12/20/14/44/meat-4708596_1280.jpg"
             },
             "amount": 500
           }
@@ -91,7 +109,8 @@ class RecipeRepository {
         "id": 3,
         "category": "Chinese",
         "name": "Spicy fried rice mix chicken bali",
-        "image": "https://cdn.pixabay.com/photo/2019/09/07/19/02/spanish-paella-4459519_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2019/09/07/19/02/spanish-paella-4459519_1280.jpg",
         "chef": "Spicy Nelly",
         "time": "20 min",
         "rating": 4.0,
@@ -100,7 +119,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 6,
               "name": "Chicken",
-              "image": "https://cdn.pixabay.com/photo/2010/12/10/08/chicken-1140_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2010/12/10/08/chicken-1140_1280.jpg"
             },
             "amount": 200
           },
@@ -108,7 +128,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 4,
               "name": "Rice",
-              "image": "https://cdn.pixabay.com/photo/2016/02/29/05/46/brown-rice-1228099_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2016/02/29/05/46/brown-rice-1228099_1280.jpg"
             },
             "amount": 150
           },
@@ -116,7 +137,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 1,
               "name": "Tomato",
-              "image": "https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg"
             },
             "amount": 100
           }
@@ -126,7 +148,8 @@ class RecipeRepository {
         "category": "Japanese",
         "id": 4,
         "name": "Ttekbokki",
-        "image": "https://cdn.pixabay.com/photo/2017/07/27/16/48/toppokki-2545943_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2017/07/27/16/48/toppokki-2545943_1280.jpg",
         "chef": "Kim Dahee",
         "time": "30 min",
         "rating": 5.0,
@@ -135,7 +158,8 @@ class RecipeRepository {
       {
         "id": 5,
         "name": "Grilled salmon with avocado salsa",
-        "image": "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg",
         "chef": "Alice Johnson",
         "time": "25 min",
         "rating": 4.5,
@@ -144,7 +168,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 5,
               "name": "Avocado",
-              "image": "https://cdn.pixabay.com/photo/2020/01/02/01/43/avocado-4734786_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2020/01/02/01/43/avocado-4734786_1280.jpg"
             },
             "amount": 150
           },
@@ -152,7 +177,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 8,
               "name": "Pepper",
-              "image": "https://cdn.pixabay.com/photo/2016/03/05/22/31/pepper-1239308_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2016/03/05/22/31/pepper-1239308_1280.jpg"
             },
             "amount": 5
           }
@@ -161,7 +187,8 @@ class RecipeRepository {
       {
         "id": 6,
         "name": "Beef Wellington",
-        "image": "https://cdn.pixabay.com/photo/2019/10/22/10/11/beef-wellington-4568239_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2019/10/22/10/11/beef-wellington-4568239_1280.jpg",
         "chef": "Gordon Ramsay",
         "time": "45 min",
         "rating": 5.0,
@@ -170,7 +197,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 2,
               "name": "Beef",
-              "image": "https://cdn.pixabay.com/photo/2016/01/21/18/08/meet-1154341_1280.png"
+              "image":
+                  "https://cdn.pixabay.com/photo/2016/01/21/18/08/meet-1154341_1280.png"
             },
             "amount": 500
           },
@@ -178,7 +206,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 9,
               "name": "Onion",
-              "image": "https://cdn.pixabay.com/photo/2013/02/21/19/14/onion-bulbs-84722_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2013/02/21/19/14/onion-bulbs-84722_1280.jpg"
             },
             "amount": 100
           }
@@ -187,7 +216,8 @@ class RecipeRepository {
       {
         "id": 7,
         "name": "Classic Margherita Pizza",
-        "image": "https://cdn.pixabay.com/photo/2019/05/15/18/56/pizza-4205701_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2019/05/15/18/56/pizza-4205701_1280.jpg",
         "chef": "Mario Batali",
         "time": "15 min",
         "rating": 4.3,
@@ -196,7 +226,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 1,
               "name": "Tomato",
-              "image": "https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg"
             },
             "amount": 200
           }
@@ -205,7 +236,8 @@ class RecipeRepository {
       {
         "id": 8,
         "name": "Sushi Platter",
-        "image": "https://cdn.pixabay.com/photo/2017/10/15/11/41/sushi-2853382_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2017/10/15/11/41/sushi-2853382_1280.jpg",
         "chef": "Jiro Ono",
         "time": "60 min",
         "rating": 4.8,
@@ -214,7 +246,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 6,
               "name": "Chicken",
-              "image": "https://cdn.pixabay.com/photo/2010/12/10/08/chicken-1140_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2010/12/10/08/chicken-1140_1280.jpg"
             },
             "amount": 200
           },
@@ -222,7 +255,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 4,
               "name": "Rice",
-              "image": "https://cdn.pixabay.com/photo/2016/02/29/05/46/brown-rice-1228099_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2016/02/29/05/46/brown-rice-1228099_1280.jpg"
             },
             "amount": 100
           }
@@ -231,7 +265,8 @@ class RecipeRepository {
       {
         "id": 9,
         "name": "French Onion Soup",
-        "image": "https://cdn.pixabay.com/photo/2016/03/03/16/19/food-1234483_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2016/03/03/16/19/food-1234483_1280.jpg",
         "chef": "Julia Child",
         "time": "40 min",
         "rating": 4.6,
@@ -240,7 +275,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 9,
               "name": "Onion",
-              "image": "https://cdn.pixabay.com/photo/2013/02/21/19/14/onion-bulbs-84722_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2013/02/21/19/14/onion-bulbs-84722_1280.jpg"
             },
             "amount": 300
           }
@@ -249,7 +285,8 @@ class RecipeRepository {
       {
         "id": 10,
         "name": "Chocolate Lava Cake",
-        "image": "https://cdn.pixabay.com/photo/2016/11/22/18/52/cake-1850011_1280.jpg",
+        "image":
+            "https://cdn.pixabay.com/photo/2016/11/22/18/52/cake-1850011_1280.jpg",
         "chef": "Paul Hollywood",
         "time": "30 min",
         "rating": 4.9,
@@ -258,7 +295,8 @@ class RecipeRepository {
             "ingredient": {
               "id": 7,
               "name": "Sugar",
-              "image": "https://cdn.pixabay.com/photo/2014/11/28/19/10/lump-sugar-549096_1280.jpg"
+              "image":
+                  "https://cdn.pixabay.com/photo/2014/11/28/19/10/lump-sugar-549096_1280.jpg"
             },
             "amount": 100
           }
@@ -267,5 +305,8 @@ class RecipeRepository {
     ]
   };
 
-
+  List<Recipe> getRecipes() {
+    final recipesJsonList = _recipeData['recipes'] as List;
+    return recipesJsonList.map((e) => Recipe.fromJson(e)).toList();
+  }
 }
